4cb8322c4e086d7d4a3cdb083fb51f7d16783e5e
Meta Engineer <m@plankc.com>
2021-08-02 12:33:26 -0500

    Adding key prop to App decorator component

--- src/containers/App/App.decorator.jsx
+++ src/containers/App/App.decorator.jsx
@@ -1,15 +1,20 @@
 import {ThemeProvider} from "@material-ui/core/styles";
 import React from "react";
 import buildTheme from "../../theme";
+import PropTypes from "prop-types";
 import App from "./App";
 
-function AppDecorator(){
+function AppDecorator({ key }){
     const theme = buildTheme();
     return (
-        <ThemeProvider theme={theme}>
+        <ThemeProvider key={key} theme={theme}>
             <App />
         </ThemeProvider>
     )
+}
+
+AppDecorator.propTypes= {
+ key: PropTypes.string.isRequired,
 }
 
 export default AppDecorator;
